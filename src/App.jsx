de81e0ccaeba4c644924454877b27dd6a57e6758
Sandra Dakey <dakeysandra@gmail.com>
2025-04-22 20:41:51 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='text-3xl underline'>Welcome to my 3d portfolio!</div>
+    </>
+  )
+}
+
+export default App;
