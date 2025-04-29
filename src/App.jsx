f227c8580fabf1f77baf844c515cffe83583aed8
Dziekhey <dakeysandra@gmail.com>
2025-04-29 22:32:37 +0000

    moved logo section into section folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "./sections/Hero";
 import Showcase from "./sections/Showcase";
 import Navbar from "./components/Navbar";
-import LogoSection from "./components/LogoSection";
+import LogoSection from "./sections/LogoSection";
 
 const App = () => {
   return (
