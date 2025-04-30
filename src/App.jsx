5d992489a68c3224514961c1d2a5fed8fa1374c4
Dziekhey <dakeysandra@gmail.com>
2025-04-30 19:28:09 +0000

    built the experience section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Showcase from "./sections/Showcase";
 import Navbar from "./components/Navbar";
 import LogoSection from "./sections/LogoSection";
 import Feature from "./sections/Feature";
+import Experience from "./sections/Experience";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Showcase />
       <LogoSection />
       <Feature />
+      <Experience />
     </>
   );
 };
