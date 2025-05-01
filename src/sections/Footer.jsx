3ac6936276a069acdd466bc6564a4848de4a8c77
Dziekhey <dakeysandra@gmail.com>
2025-05-01 02:14:54 +0000

    built footer section

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { socialImgs } from "../constants";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="flex flex-col justify-center items-center md:items-start">
+            <a href="/">Visit my blog</a>
+          </div>
+          <div className="socials">
+            {socialImgs.map((img, index) => (
+              <a className="icon" target="_blank" href={img.url} key={index}>
+                <img src={img.imgPath} />
+              </a>
+            ))}
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-center md:text-end">
+              © {new Date().getFullYear()} Sandra. All rights reserved
+            </p>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
